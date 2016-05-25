30ca1f0a8b60ca44e9dfd1ede1f55ceaefd9ae31
Christophe <christophe.meyers@outlook.com>
2016-05-25 20:15:07 +0200

    Starting the login page

--- functions.php
+++ functions.php
@@ -9,12 +9,21 @@
 //include("dbConnection.php");
 
 
+
+
 function user_registered($email,$password) {
     //test to discover if the user is already in the DB
     //to do that, we can find out if the email address already exists in any row
 
 //    include("db_connection.php");
     echo "<script>alert('Test 1');</script>";
+
+    $db = new MySQLi(
+        'ap-cdbr-azure-east-c.cloudapp.net', //server or host address
+        'b27f975a706fe7', //username for connecting to database
+        '078b0d65', //user's password
+        'meyerseuro16bets' //database being connected to
+    );
 
     if($db->connect_errno){		//check if there was a connection error and respond accordingly
         die('Connection failed:'.connect_error);
